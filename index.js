2357f30a4b7c8c10be5258abde3d472d27d60d73
Herland Mojica Cardozo <mojicacardozoh@gmail.com>
2024-08-27 10:59:57 -0400

    commit day 192

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ console.log("hola mundo x15");
 console.log("hola mundo x16");
 console.log("hola mundo x17");
 console.log("hola mundo x18");
-console.log("hola mundo x19");
+console.log("hola mundo x192");
 
 
 
