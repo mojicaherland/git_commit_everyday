525b6d48bb91b803c6834a5e4c701df1e1b10554
Herland Mojica Cardozo <mojicacardozoh@gmail.com>
2025-07-06 19:15:59 -0400

    Actualizar index.java
    hoy será el último dia en qué hablaremos y dónde todo se acabara, o bueno me voy a desaparecer(me voy a matar, lo intentaré

---
+++ index.java
@@ -0,0 +1,12 @@
+public static void Main(String args[]){
+      console.log("hola mundo");
+}
+
+Public Class Persona{
+   String nombre;
+   String apellido;
+   int edad;
+}
+public void saludar(){
+    console.log("Hola esto es un saludo");
+}
